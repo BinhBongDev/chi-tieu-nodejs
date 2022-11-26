54871905c4fdf7fc3d10ce93c32c66634db40b12
BinhBongDev <hoangvanbinht37@gmail.com>
2022-11-26 16:14:41 +0700

    7. basic header, nav

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,12 +1,23 @@
 const menuIcon = document.getElementById('menuIcon')
 const exitIcon = document.getElementById("exitIcon")
+const navMobile = document.querySelector('.mobile-nav')
+// const backdrop = document.querySelector('.backdrop')
 
-menuIcon.onclick = () => {
+const openIcon = () => {
     exitIcon.style.display = 'block'
+    navMobile.style.display = 'block'
+    // backdrop.style.display = 'block'
     menuIcon.style.display = 'none'
 }
 
-exitIcon.onclick = () => {
+const closeIcon = () => {
     menuIcon.style.display = 'block'
     exitIcon.style.display = 'none'
+    navMobile.style.display = 'none'
+    // backdrop.style.display = 'none'
+
 }
+
+menuIcon.addEventListener('click', openIcon)
+exitIcon.addEventListener('click',closeIcon)
+// backdrop.addEventListener('click', closeIcon)
