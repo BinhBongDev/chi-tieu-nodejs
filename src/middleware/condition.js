8875db45c22776842c1e2b5a7b8997f8b1c2f7ed
BinhBongDev <hoangvanbinht37@gmail.com>
2022-11-28 17:02:46 +0700

    20. logic UI show/hide signup/login

--- src/middleware/condition.js
+++ src/middleware/condition.js
@@ -1,7 +1,9 @@
 const takeCookie = (req, res, next) => {
-    const cookiesArray = req.headers.cookie.split(';')[0].split("=")[1];
-    if(cookiesArray === "true") {
-        req.isLoggedin = true
+    if(req.headers.cookie) {
+        const cookiesArray = req.headers.cookie.split(';')[0].split("=")[1];
+        if(cookiesArray === "true") {
+            req.isLoggedin = true
+        }
     } else {
         res.redirect('/dang-ky')
     }
